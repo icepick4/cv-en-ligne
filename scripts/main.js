550891ee8be55608046ca237176c31d54f81d543
icepick4 <remi.jara@laposte.net>
2022-04-15 13:00:25 +0200

    change path of index for gh pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,1 @@
-/*hover id*/
-$(document).ready(function(){
-    $(".hover").mouseover(function(){
-        $(this).addClass("hover");
-    });
-    $(".hover").mouseout(function(){
-        $(this).removeClass("hover");
-    });
-}
+
